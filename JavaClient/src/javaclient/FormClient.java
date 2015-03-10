a4a350a7487a0d5a17058f3b183470e1056d657b
FaisZ <fais9999@ymail.com>
2015-03-10 16:17:47 +0700

    Basic client connect

--- JavaClient/src/javaclient/FormClient.java
+++ JavaClient/src/javaclient/FormClient.java
@@ -122,7 +122,7 @@ public class FormClient extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
--- JavaClient/src/javaclient/JavaClient.java
+++ JavaClient/src/javaclient/JavaClient.java
@@ -4,16 +4,43 @@
  */
 package javaclient;
 
-/**
- *
- * @author F
- */
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class JavaClient {
 
-    /**
-     * @param args the command line arguments
-     */
+    private String hostname;
+    private int port;
+    Socket sockcli;
+    
+    public JavaClient(String hostname, int port){
+        this.hostname = hostname;
+        this.port = port;
+    }
+    
+    public void connect(){
+        System.out.println("Connecting to "+hostname+":"+port);
+        try {
+            sockcli = new Socket(hostname,port);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(JavaClient.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JavaClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Connected!");
+    }
+    
+    public void rdResponse(){
+        //read from server here
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        JavaClient sockclient = new JavaClient("localhost",8080);
+        sockclient.connect();
+        sockclient.rdResponse();
     }
 }
