0e3d0297aff83ddddeefa57d315e6404edf2820c
FaisZ <fais9999@ymail.com>
2015-04-14 16:31:52 +0700

    Template AES DFB Daniel

---
+++ ClientFix/src/AES.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author F
+ */
+public class AES {
+    private int[][] S_Box;
+    private int[][] Inverse_S_Box;
+    
+    public AES()
+    {
+        
+    }
+    
+    public String encrypt(String plaintext, String key)
+    {
+        return null;
+    }
+    
+    public String decrypt(String ciphertext, String key)
+    {
+        return null;
+    }
+}
---
+++ ClientFix/src/CFB.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author F
+ */
+public class CFB {
+    private int N;
+
+    public String encrypt(String plaintext, String key)
+    {
+        return null;
+        
+    }
+    
+    public String decrypt(String ciphertext, String key)
+    {
+        return null;
+        
+    }
+}
