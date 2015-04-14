8a8e3688f093f285fddafd6cdaa6cc3f688b0263
ZolaMajendra <gianfzola@gmail.com>
2015-04-14 22:43:38 +0700

    Update AES.java
    Update AES.java

--- ClientFix/src/AES.java
+++ ClientFix/src/AES.java
@@ -1,28 +1,245 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package ChatRoom;
+import java.math.*;
+import java.nio.charset.Charset;
+import java.lang.Object;
 /**
  *
- * @author F
+ * @author Ahmad Mustofa
  */
 public class AES {
-    private int[][] S_Box;
-    private int[][] Inverse_S_Box;
-    
-    public AES()
+    static String iv= "aefjnostvzAEGIMO";    
+    char[][] ip = new char[16][2];
+    private static char[][] sbox = 
     {
-        
+        {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76},
+        {0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0},
+        {0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15},
+        {0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75},
+        {0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84},
+        {0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF},
+        {0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8},
+        {0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2},
+        {0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73},
+        {0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB},
+        {0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79},
+        {0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08},
+        {0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A},
+        {0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E},
+        {0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF},
+        {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}
+    };
+    private static char[][] inversSbox = 
+    {
+        {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76},
+        {0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0},
+        {0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15},
+        {0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75},
+        {0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84},
+        {0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF},
+        {0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8},
+        {0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2},
+        {0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73},
+        {0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB},
+        {0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79},
+        {0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08},
+        {0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A},
+        {0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E},
+        {0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF},
+        {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}
+    };
+    public static String toHex(String arg) 
+    {
+        return String.format("%2x", new BigInteger(1, arg.getBytes(/*YOUR_CHARSET?*/)));
     }
-    
-    public String encrypt(String plaintext, String key)
+    public static char[][] InputState(String iv)
     {
-        return null;
+        char[] tmp = iv.toCharArray();
+        char[][]out = new char[4][4];
+        int x;
+        for(int i = 0;i<4;i++)
+        {
+            x = i;
+            for(int j = 0;j<4;j++)
+            {
+                out[i][j] = tmp[x];
+                x+=4;
+            }
+        }
+        return out;
     }
-    
-    public String decrypt(String ciphertext, String key)
+    private static char[][] StateBox(char[][] InputState, String status)
     {
-        return null;
+        String tmp,x,y;
+        char[] temp;
+        char[][] box = new char[16][16];
+        if(status.equalsIgnoreCase("enkripsi"))
+            box = sbox.clone();
+        else if(status.equalsIgnoreCase("dekripsi"))
+            box = inversSbox.clone();
+        for(int i = 0;i<4;i++)
+        {
+            for(int j = 0;j<4;j++)
+            {
+                tmp = toHex(Character.toString(InputState[i][j]));
+                temp = tmp.toCharArray();
+                x = Character.toString(temp[0]);
+                //System.out.println("x = "+Integer.valueOf(x, 16));
+                y = Character.toString(temp[1]);
+                //System.out.println("y = "+Integer.valueOf(y, 16));
+                InputState[i][j] = box[Integer.valueOf(x, 16)][Integer.valueOf(y,16)];
+            }
+        }
+        return InputState;
+    }
+    private static char[][] ShiftRow(char[][] stateBox,String status)
+    {
+        char[][] out = new char[4][4];
+        for(int i = 0; i < 4;i++)
+        {
+            System.arraycopy(stateBox[i], 0, out[i], 0, stateBox.length);
+        }
+        if(status.equalsIgnoreCase("enkripsi"))
+        {
+            stateBox[1][0] = out[1][1];
+            stateBox[1][1] = out[1][2];
+            stateBox[1][2] = out[1][3];
+            stateBox[1][3] = out[1][0];
+            stateBox[2][0] = out[2][2];
+            stateBox[2][1] = out[2][3];
+            stateBox[2][2] = out[2][0];
+            stateBox[2][3] = out[2][1];
+            stateBox[3][0] = out[3][3];
+            stateBox[3][1] = out[3][0];
+            stateBox[3][2] = out[3][1];
+            stateBox[3][3] = out[3][2];
+        }
+        else if(status.equalsIgnoreCase("dekripsi"))
+        {
+            stateBox[1][0] = out[1][3];
+            stateBox[1][1] = out[1][0];
+            stateBox[1][2] = out[1][1];
+            stateBox[1][3] = out[1][2];
+            stateBox[2][0] = out[2][2];
+            stateBox[2][1] = out[2][3];
+            stateBox[2][2] = out[2][0];
+            stateBox[2][3] = out[2][1];
+            stateBox[3][0] = out[3][1];
+            stateBox[3][1] = out[3][2];
+            stateBox[3][2] = out[3][3];
+            stateBox[3][3] = out[3][0];
+        }
+        return stateBox;
+    }
+    private static char GaloisField(int x,char y)
+    {
+        int tmp;
+        String biner = Integer.toBinaryString((int)y);
+        if(biner.length()<8)
+            biner = '0'+biner;
+        char pertama = biner.charAt(0);
+        if(x == 2)
+        {
+            biner = biner.substring(1,biner.length())+'0';
+            if(pertama == '1')
+            {
+                tmp = Integer.parseInt(biner, 2);
+                tmp = tmp^27;
+                return (char)tmp;
+            }
+            return (char)Integer.parseInt(biner,2);
+        }
+        else if(x == 3)
+        {
+            y = (char)(y^GaloisField(2,y));
+        }
+        return y;
+    }
+    private static char[][] MixColumn(char[][] shiftRow,String status)
+    {
+        char[][] out = new char[4][4];
+        for(int i = 0; i < 4;i++)
+        {
+            System.arraycopy(shiftRow[i], 0, out[i], 0, shiftRow.length);
+        }
+        if(status.equalsIgnoreCase("enkripsi"))
+        {
+            for(int i = 0; i < 4;i++)
+            {
+                for(int j = 0; j < 4;j++)
+                {
+                    if(i == 0)
+                    {
+                        out[i][j] = (char)((int)GaloisField(2,shiftRow[0][j])^(int)GaloisField(3,shiftRow[1][j])^(int)shiftRow[2][j]^(int)shiftRow[3][j]);
+                    }
+                    else if(i==1)
+                    {
+                        out[i][j] = (char)((int)shiftRow[0][j]^(int)GaloisField(2,shiftRow[1][j])^(int)GaloisField(3,shiftRow[2][j])^(int)shiftRow[3][j]);
+                    }
+                    else if(i==2)
+                    {
+                        out[i][j] = (char)((int)shiftRow[0][j]^(int)shiftRow[1][j]^(int)GaloisField(2,shiftRow[2][j])^(int)GaloisField(3,shiftRow[3][j]));
+                    }
+                    else
+                    {
+                        out[i][j] = (char)((int)GaloisField(3,shiftRow[0][j])^(int)shiftRow[1][j]^(int)shiftRow[2][j]^(int)GaloisField(2,shiftRow[3][j]));
+                    }
+                }
+            }
+        }
+        else if(status.equalsIgnoreCase("dekripsi"))
+        {
+            
+        }
+        return out;
+    }
+    public static void main(String[] args)
+    {
+        String coba = "abcdefghijklmnop",tmp;
+        char [][] out = InputState(coba).clone(),m = new char[4][4];
+        String x;
+        for(int i = 0; i < 4;i++)
+        {
+            for(int j = 0; j < 4;j++)
+                System.out.print(out[i][j]+" ");
+            System.out.println();
+        }
+        out = StateBox(out,"enkripsi");
+        for(int i = 0; i < 4;i++)
+        {
+            for(int j = 0; j < 4;j++)
+            {
+                x = String.format("%2x", (int)out[i][j]);
+                System.out.println(x);
+            }
+            System.out.println();
+        }
+        out = ShiftRow(out,"enkripsi");
+        for(int i = 0; i < 4;i++)
+        {
+            for(int j = 0; j < 4;j++)
+            {
+                x = String.format("%2x", (int)out[i][j]);
+                System.out.println(x);
+            }
+            System.out.println();
+        }
+        out = MixColumn(out,"enkripsi");
+        for(int i = 0; i < 4;i++)
+        {
+            for(int j = 0; j < 4;j++)
+            {
+                x = String.format("%2x", (int)out[i][j]);
+                System.out.println(x);
+            }
+            System.out.println();
+        }
+        //char m = GaloisField(2,'a');
+        //biner = biner.substring(1,biner.length())+'0';
+        //System.out.println(m);*/
     }
 }
