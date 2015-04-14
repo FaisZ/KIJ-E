3d705ca60ebaee254383661c93da406251e4bd76
ZolaMajendra <gianfzola@gmail.com>
2015-04-15 00:04:50 +0700

    RSA Generate Key
    RSA Generate Key (Public key - Private key)

--- ClientFix/src/RSA.java
+++ ClientFix/src/RSA.java
@@ -1,90 +1,38 @@
-import java.math.BigInteger; 
-import java.util.Random;
-import java.io.*;
-import java.nio.charset.Charset;
-
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ChatRoom;
 
 /**
  *
- * @author F
+ * @author Vanjul
  */
 
-// Encrypt message
-public class RSA {
-
-    private BigInteger p; 
-    private BigInteger q; 
-    private BigInteger N; 
-    private BigInteger phi; 
-    private BigInteger e; 
-    private BigInteger d; 
-    private int bitlength = 1024; 
-    private int blocksize = 256; //blocksize in byte 
-     
-    private Random r; 
-     
-    public RSA() 
-    { 
-        r = new Random(); 
-        p = BigInteger.probablePrime(bitlength, r); 
-        q = BigInteger.probablePrime(bitlength, r); 
-        N = p.multiply(q); 
-           
-        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)); 
-        e = BigInteger.probablePrime(bitlength/2, r); 
-         
-        while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0 ) 
+import java.math.BigInteger; 
+import java.util.Random;
+import java.io.*;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import javax.crypto.Cipher;
+  
+public class RSA { 
+        
+        public static void main (String[] args) throws IOException, NoSuchAlgorithmException
         { 
-            e.add(BigInteger.ONE); 
-        } 
+            final KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(512);
+            final KeyPair key = keyGen.generateKeyPair();
 
-        d = e.modInverse(phi);  
-    } 
-    
-    public RSA(BigInteger e, BigInteger d, BigInteger N) 
-    { 
-        this.e = e; 
-        this.d = d; 
-        this.N = N; 
-    } 
-     
-    public static String RSA_Encryption (String AESKey)
-{ 
-    RSA rsa = new RSA(); 
-    DataInputStream in=new DataInputStream(System.in);  
+            String pub = key.getPublic().toString();
+            String pri = key.getPrivate().toString();
 
-    // encrypt 
-    byte[] encrypted = rsa.encrypt(AESKey.getBytes());                   
+            System.out.println("public key: "+pub+" - " + pub.length());
+            System.out.println("private key: "+pri+" - " + pri.length());
 
-    // decrypt 
-    byte[] decrypted = rsa.decrypt(encrypted);       
-
-    return (bytesToString(decrypted));
-} 
-
-    private static String bytesToString(byte[] encrypted) 
-    { 
-        String test = ""; 
-        for (byte b : encrypted) 
-        { 
-            test += Byte.toString(b); 
-        } 
-        return test; 
-    } 
-   
-    // Encrypt key
-    public byte[] encrypt(byte[] AESKey) 
-    {
-        return (new BigInteger(AESKey)).modPow(e, N).toByteArray(); 
-    } 
-       
-    // Decrypt key
-    public byte[] decrypt(byte[] AESKey) 
-    {
-        return (new BigInteger(AESKey)).modPow(d, N).toByteArray(); 
-    }    
+        }    
 }
