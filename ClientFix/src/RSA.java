9f0600dd42849cf0df50f984f44347cc91b1a4a0
FaisZ <fais9999@ymail.com>
2015-04-14 21:42:53 +0700

    RSA Update

--- ClientFix/src/RSA.java
+++ ClientFix/src/RSA.java
@@ -1,6 +1,7 @@
 import java.math.BigInteger; 
 import java.util.Random;
 import java.io.*;
+import java.nio.charset.Charset;
 
 /*
  * To change this template, choose Tools | Templates
@@ -11,16 +12,79 @@ import java.io.*;
  *
  * @author F
  */
+
 // Encrypt message
 public class RSA {
-    public byte encrypt(String AESKey) 
-    {
-        return null;
-        
-    }       
-// Decrypt message
-    public String decrypt(byte[] AESKey) 
+
+    private BigInteger p; 
+    private BigInteger q; 
+    private BigInteger N; 
+    private BigInteger phi; 
+    private BigInteger e; 
+    private BigInteger d; 
+    private int bitlength = 1024; 
+    private int blocksize = 256; //blocksize in byte 
+     
+    private Random r; 
+     
+    public RSA() 
     { 
-        return null;
-    }  
+        r = new Random(); 
+        p = BigInteger.probablePrime(bitlength, r); 
+        q = BigInteger.probablePrime(bitlength, r); 
+        N = p.multiply(q); 
+           
+        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)); 
+        e = BigInteger.probablePrime(bitlength/2, r); 
+         
+        while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0 ) 
+        { 
+            e.add(BigInteger.ONE); 
+        } 
+
+        d = e.modInverse(phi);  
+    } 
+    
+    public RSA(BigInteger e, BigInteger d, BigInteger N) 
+    { 
+        this.e = e; 
+        this.d = d; 
+        this.N = N; 
+    } 
+     
+    public static String RSA_Encryption (String AESKey)
+{ 
+    RSA rsa = new RSA(); 
+    DataInputStream in=new DataInputStream(System.in);  
+
+    // encrypt 
+    byte[] encrypted = rsa.encrypt(AESKey.getBytes());                   
+
+    // decrypt 
+    byte[] decrypted = rsa.decrypt(encrypted);       
+
+    return (bytesToString(decrypted));
+} 
+
+    private static String bytesToString(byte[] encrypted) 
+    { 
+        String test = ""; 
+        for (byte b : encrypted) 
+        { 
+            test += Byte.toString(b); 
+        } 
+        return test; 
+    } 
+   
+    // Encrypt key
+    public byte[] encrypt(byte[] AESKey) 
+    {
+        return (new BigInteger(AESKey)).modPow(e, N).toByteArray(); 
+    } 
+       
+    // Decrypt key
+    public byte[] decrypt(byte[] AESKey) 
+    {
+        return (new BigInteger(AESKey)).modPow(d, N).toByteArray(); 
+    }    
 }
