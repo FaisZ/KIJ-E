060e22d85336b2ddaeb839616613922a50cdeef4
FaisZ <fais9999@ymail.com>
2015-04-14 21:02:19 +0700

    RSA

---
+++ ClientFix/src/RSA.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger; 
+import java.util.Random;
+import java.io.*;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author F
+ */
+// Encrypt message
+public class RSA {
+    public byte encrypt(String AESKey) 
+    {
+        return null;
+        
+    }       
+// Decrypt message
+    public String decrypt(byte[] AESKey) 
+    { 
+        return null;
+    }  
+}
